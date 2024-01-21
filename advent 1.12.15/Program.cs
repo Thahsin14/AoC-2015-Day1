f50a9f229bdf7c27ad45270c0889f5b889f9b9e2
Puczek <Puczek@DESKTOP-LEE8JM2>
2024-01-21 22:43:06 +0100

    Add project files.

---
+++ advent 1.12.15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+
+string readText = File.ReadAllText("../../../input.txt");
+int up = 0, down = 0, floor = 0;
+bool flag = false;
+for (int i = 0; i < readText.Length; i++)
+{
+    if (readText[i] == '(')
+    {
+        up++;
+        floor++;
+    }
+    else
+    {
+        down++;
+        floor++;
+    }
+    if (up - down == -1 && flag == false)
+    {
+        Console.WriteLine("part two = " + floor);
+        flag = true;
+
+    }
+}
+Console.WriteLine("part one = " + (up - down));
+
